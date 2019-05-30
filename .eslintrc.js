9dddf65ed959be85aac492adaead2a19695f3820
Christopher Bonhage <chris@mix.com>
2019-05-30 17:04:13 -0400

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+    }
+};
